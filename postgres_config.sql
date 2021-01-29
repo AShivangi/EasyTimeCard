7e062e7391cf15a0f9524097045657d0ae486027
Shivangi Agarwal <shivangiagarwal53@gmail.com>
2021-01-29 12:05:40 -0800

    Update postgres_config.sql

--- postgres_config.sql
+++ postgres_config.sql
@@ -1,4 +1,4 @@
-CREATE ROLE django WITH LOGIN PASSWORD 'se172fun';
+/*CREATE ROLE django WITH LOGIN PASSWORD ''; */
 ALTER ROLE django SET client_encoding TO 'utf8';
 ALTER ROLE django SET default_transaction_isolation TO 'read committed';
 ALTER ROLE django SET timezone TO 'UTC'; 
